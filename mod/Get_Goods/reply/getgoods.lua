ffce4dd5257832584ee4cd69a984184194495705
wangjunyue <wangjunyue@babeltime.com>
2022-11-24 18:40:40 +0800

    更新催肾功能

--- mod/Get_Goods/reply/getgoods.lua
+++ mod/Get_Goods/reply/getgoods.lua
@@ -36,7 +36,7 @@ msg_reply.Move_Goods = {	--该条msg_reply的id，唯一对应，同名覆盖
 
 msg_reply.Push_Payment = {	--该条msg_reply的id，唯一对应，同名覆盖
     keyword = {
-        prefix = "催肾",
+        prefix = "&催肾",
     },
     echo = {
         lua = "reply_push_payments"	--调用文件名对应lua
--- mod/Get_Goods/script/reply_push_payments.lua
+++ mod/Get_Goods/script/reply_push_payments.lua
@@ -1,13 +1,18 @@
 notice_head = ".send qq"
-local text = string.sub(msg.fromMsg,#"uin"+2)
 
-local message = string.sub(text,string.find(text,"消息：")+9,"-1")
+local text = string.match(msg.fromMsg,"^[%s]*(.-)[%s]*$",#"&催肾"+1)
+
+local message = string.sub(text, 1, string.find(text,"at")-5)
+
+local atPplGroup = string.sub(text, string.find(text,"at"), string.find(text,"image"))
+
+local image = string.sub(text, string.find(text,"image")-4)
 
 qqnums = {}
-for qqnum in string.gmatch(text, "%d+") do
+for qqnum in string.gmatch(atPplGroup, "%d+") do
     table.insert(qqnums, qqnum.." ")
-    eventMsg(notice_head..qqnum.." "..message, msg.fromGroup, "674037656")
+    eventMsg(notice_head..qqnum.." "..message..image, msg.fromGroup, "674037656")
     sleepTime(5000)
 end
+
 return "已向"..table.concat(qqnums).."催肾"
-    
