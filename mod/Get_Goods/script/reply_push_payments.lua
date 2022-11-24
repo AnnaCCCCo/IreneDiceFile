3fdcd87d0d7760ffa254aaf4fcff82365f6ca594
wangjunyue <wangjunyue@babeltime.com>
2022-11-24 17:53:35 +0800

    backup 11.24

--- mod/Get_Goods/script/reply_push_payments.lua
+++ mod/Get_Goods/script/reply_push_payments.lua
@@ -6,7 +6,7 @@ local message = string.sub(text,string.find(text,"消息：")+9,"-1")
 qqnums = {}
 for qqnum in string.gmatch(text, "%d+") do
     table.insert(qqnums, qqnum.." ")
-    eventMsg(notice_head..qqnum.." "..message, "277649597", "674037656")
+    eventMsg(notice_head..qqnum.." "..message, msg.fromGroup, "674037656")
     sleepTime(5000)
 end
 return "已向"..table.concat(qqnums).."催肾"
