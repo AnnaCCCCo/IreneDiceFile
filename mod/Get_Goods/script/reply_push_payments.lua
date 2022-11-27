c304bffa99888f99a87b29f3e149dc64b35cbdcf
wangjunyue <wangjunyue@babeltime.com>
2022-11-27 09:59:19 +0800

    backup 11.27

--- mod/Get_Goods/script/reply_push_payments.lua
+++ mod/Get_Goods/script/reply_push_payments.lua
@@ -6,12 +6,19 @@ local message = string.sub(text, 1, string.find(text,"at")-5)
 
 local atPplGroup = string.sub(text, string.find(text,"at"), string.find(text,"image"))
 
-local image = string.sub(text, string.find(text,"image")-4)
+if string.find(text,"image") then
+    image = string.sub(text, string.find(text,"image")-4)
+end
 
 qqnums = {}
 for qqnum in string.gmatch(atPplGroup, "%d+") do
     table.insert(qqnums, qqnum.." ")
-    eventMsg(notice_head..qqnum.." "..message..image, msg.fromGroup, "674037656")
+    if string.find(text,"image") then
+        eventMsg(notice_head..qqnum.." "..message..image, msg.fromQQ, "674037656")
+    else
+        eventMsg(notice_head..qqnum.." "..message, msg.fromQQ, "674037656")
+    end
+    
     sleepTime(5000)
 end
 
